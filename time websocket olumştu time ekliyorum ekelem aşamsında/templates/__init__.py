dd86bc38c6e700594c9ea01e45f5f05f30ba13e5
asitann9-cyber <asitann9@gmail.com>
2025-11-23 18:48:00 +0300

    Add files via upload

---
+++ time websocket olumştu time ekliyorum ekelem aşamsında/templates/__init__.py
@@ -0,0 +1,8 @@
+"""
+Templates package
+Flask HTML template'leri ve statik dosyalar
+"""
+
+__all__ = []
+__version__ = '1.0.0'
+__description__ = 'HTML templates for Consecutive Candles Analysis System'
